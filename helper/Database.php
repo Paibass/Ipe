c539a3b64490550379f065766bed700bc4532e63
Paiva <85632116+Paibass@users.noreply.github.com>
2024-07-07 10:41:20 -0300

    Update Database.php

--- helper/Database.php
+++ helper/Database.php
@@ -4,9 +4,9 @@ class Database
 {
     private $conn;
 
-    public function __construct($servername, $username, $password, $dbname)
+    public function __construct($servername, $username, $password, $dbname, $port)
     {
-        $this->conn = mysqli_connect($servername, $username, $password, $dbname);
+        $this->conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
         if (!$this->conn) {
             die("Connection failed: " . mysqli_connect_error());
